a3e1e214edbe8822599684fdfac4c99ffd456ce7
Osvaldo Beto <osvaldogui744@gmail.com>
2024-04-08 21:55:45 +0100

    aaaaaaaaaaaaaaaaaaaa

--- setup/compliance/pack_physical_person/physicalModel.py
+++ setup/compliance/pack_physical_person/physicalModel.py
@@ -1,11 +1,12 @@
 import conection.connect as connecao
 
 ############ Listando Physical_person ###############
-def listar_physical():
+def listar_physical(pp_email):
     try:
         connection = connecao.cria_connecao()
         cursor = connection.cursor()
-        cursor.execute(""" SELECT * FROM tb_physical_person WHERE pp_name = %s AND pp_email = %s AND pp_type = %s AND pp_senha = %s """)
+        cursor.execute(""" SELECT pp_name, pp_email, pp_type, pp_senha FROM 
+        tb_physical_person WHERE pp_email = %s """, (pp_email))
         dados = cursor.fetchall()
         dados_novo = [item[1] for item in dados]
         cursor.close()
